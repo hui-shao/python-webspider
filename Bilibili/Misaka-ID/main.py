9aec0de6b038775aaef85cf25b590ba2c5ab2238
hui-shao <31568606+hui-shao@users.noreply.github.com>
2020-03-31 19:51:57 +0800

    新增 异常自动重试 消息推送
    另外：
    修正 read.py 正则表达式的一个错误
    更新数据（list.txt）

--- Bilibili/Misaka-ID/main.py
+++ Bilibili/Misaka-ID/main.py
@@ -3,8 +3,87 @@ import json
 import time
 import random
 
+
+def send_wxmsg(_sckey, _title="misaka", _context="正文"):
+    url = "https://sc.ftqq.com/%s.send" % (_sckey)
+    _context = _context + "\n\n" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    data = {
+        "text": "%s" % (_title),
+        "desp": "%s" % (_context)
+    }
+    try:
+        res = requests.post(url=url, data=data)
+        msg_back = json.loads(res.text)
+        if msg_back["errmsg"] == "success":
+            print("消息推送成功！")
+        else:
+            print("发送可能失败 返回值：%s" % (msg_back["errmsg"]))
+    except Exception:
+        print("消息发送错误")
+
+
+def check():
+    retry_n = 0
+    error_status = 0
+    while retry_n <= 3:
+        if retry_n != 0:
+            print("\nRetry:%d times:%d  :" % (i, retry_n))
+        try:
+            res = requests.get(url=url, params=par, headers=hea, timeout=10)
+        except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+            print(i, "timeout")
+            error_status = 1
+            retry_n += 1
+            time.sleep(2)
+        except Exception as err_info:
+            print("%d Error！\n" % i, err_info)
+            error_status = 2
+            retry_n += 1
+            time.sleep(5)
+        else:
+            error_status = 0
+            result = json.loads(res.text)
+            if result["code"] == 0:
+                print(i, "available")
+                lst_available.append(i)
+            else:
+                print(i, "unavailable")
+                lst_unavailable.append(i)
+            break
+    if error_status == 1:
+        timeout.append(i)
+    elif error_status == 2:
+        errs.append(i)
+    if i % 20 == 0:
+        # 每检测20个写一次文件
+        f.seek(0)
+        f.write("Available: " + str(lst_available) + "\nUnavailable: " + str(lst_unavailable) + "\nTimeout: " + str(timeout) + "\nError: " + str(errs) + "\n\nAvailable_count = %d\nUnavailable_count = %d\n" % (len(lst_available), len(lst_unavailable)))
+        f.flush()
+
+
+def sleep():
+    if i % 100 == 0:
+        sleep_time_1 = random.randint(5, 15)
+        print("\n达到整百，随机暂停 %d s\n" % sleep_time_1)
+        time.sleep(sleep_time_1)
+    elif i % 10 == 0:
+        time.sleep(random.uniform(0.2, 0.6))  # 取0.2-0.7之间的随机float
+    else:
+        time.sleep(0.02)
+
+
+def loop():
+    global i, par
+    for i in range(1, 20002):
+        par = {"nickName": "御坂" + str(i) + "号"}  # request.get 参数
+        check()
+        sleep()
+
+
 if __name__ == "__main__":
-    start = time.time()
+    # Some vars
+    sckey = ""  # 用于 servre酱 消息推送，若不需要，无需修改保持现状
+    start_time = time.time()
     lst_available = []
     lst_unavailable = []
     timeout = []
@@ -17,45 +96,19 @@ if __name__ == "__main__":
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "zh-CN,zh;q=0.9"
     }
+    par = {"nickName": "御坂1号"}
+    i = 1
+    # run
     f = open("./lists.txt", "w+", encoding="utf-8")
-    for i in range(1, 20002):
-        par = {
-            "nickName": "御坂" + str(i) + "号"
-        }
-        try:
-            res = requests.get(url=url, params=par, headers=hea, timeout=5)
-        except KeyboardInterrupt:
-            print("Cancled")
-            break
-        except requests.exceptions.ConnectTimeout:
-            print("timeout", i)
-            timeout.append(i)
-            continue
-        except Exception as err:
-            print("Error！ %d\n" % i, err)
-            errs.append(i)
-            continue
-        else:
-            result = json.loads(res.text)
-            if result["code"] == 0:
-                print("available", i)
-                lst_available.append(i)
-            else:
-                print("unavailable", i)
-                lst_unavailable.append(i)
-        a = str(lst_available)
-        f.seek(0)
-        f.write("Available: " + str(lst_available) + "\nUnavailable: " + str(lst_unavailable) + "\nTimeout: " + str(
-            timeout) + "\nError: " + str(errs))
-        f.write("\n\nAvailable_count = %d\nUnavailable_count = %d" % (len(lst_available), len(lst_unavailable)))
-        if i % 500 == 0:
-            f.flush()
-        if i % 100 == 0:
-            sleep_time_1 = random.randint(10, 30)
-            print("\n达到整百，随机暂停 %d s\n" % sleep_time_1)
-            time.sleep(sleep_time_1)
-        else:
-            time.sleep(random.uniform(0.1, 0.5))  # 取0.1-0.5之间的随机float
+    try:
+        loop()  # 进入 for 循环
+    except KeyboardInterrupt:
+        print("\nRaised Control-C.  Cancled!\n")
+    except Exception as err_info_1:
+        print("\nError!\n", err_info_1)
+    else:
+        total_time = time.time() - start_time
+        print("\n\nFinished\nTotal time: %f\n" % total_time)
+        if not sckey == "":
+            send_wxmsg(_sckey=sckey, _title="Misaka-ID", _context="Finished.\n\nTotal time: %f" % total_time)
     f.close()
-    end = time.time()
-    print("\n\nFinished.  Total time:", end - start)
--- Bilibili/Misaka-ID/read.py
+++ Bilibili/Misaka-ID/read.py
@@ -1,24 +1,39 @@
 import re
 import ast
 import traceback
+import sys
 
-if __name__ == "__main__":
+
+def run():
     # 从文件中读取
-    f = open("./lists.txt", "r", encoding="utf-8")
-    lines = f.readlines()
-    f.close()
+    try:
+        f = open("./lists.txt", "r", encoding="utf-8")
+        lines = f.readlines()
+        f.close()
+    except FileNotFoundError:
+        print("list.txt 不存在！")
+        sys.exit()
+    except Exception:
+        print("ERROR!\n", traceback.format_exc())
+        sys.exit()
     # 字符操作
     avai = lines[0].strip("\n")
     unavai = lines[1].strip("\n")
     avai = re.sub(r"^.{9}: ", "", avai)
-    unavai = re.sub(r"^.{10}: ", "", unavai)
+    unavai = re.sub(r"^.{11}: ", "", unavai)
     # 把列表形式的字符串转为列表
     try:
         available = ast.literal_eval(avai)
         unavailable = ast.literal_eval(unavai)
     except Exception:
         print("ERROR!\n", traceback.format_exc())
+        sys.exit()
     else:
         # 计数
         print("已注册数：", len(unavailable))
         print("未注册数：", len(available))
+    return unavailable, available
+
+
+if __name__ == "__main__":
+    run()
